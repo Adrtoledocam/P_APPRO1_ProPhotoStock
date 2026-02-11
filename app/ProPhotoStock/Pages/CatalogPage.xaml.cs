729725a8a9657d9b0717f77065aca905588ecb0d
Adrtoledocam <pq17tmn@gmail.com>
2026-02-11 15:10:51 +0100

    Feat : Page Catalogue basic fait

--- app/ProPhotoStock/Pages/CatalogPage.xaml.cs
+++ app/ProPhotoStock/Pages/CatalogPage.xaml.cs
@@ -5,5 +5,25 @@ public partial class CatalogPage : ContentPage
 	public CatalogPage()
 	{
 		InitializeComponent();
-	}
+
+        PhotosList.ItemsSource = new List<PhotoItem> 
+		{ 
+			new PhotoItem 
+			{ 
+				User = "Utilisateur 1", 
+				Title = "Photo 1", 
+				ImageUrl = "https://images.unsplash.com/photo-1506744038136-46273834b3fb", 
+				Tag = "#Paysage" 
+			}, 
+			new PhotoItem 
+			{ 
+				User = "Utilisateur 2", 
+				Title = "Photo 2", 
+				ImageUrl = "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+				Tag = "#Portrait" 
+			} 
+		};
+    }
 }
+
+public class PhotoItem { public required string Title { get; set; } public required string User { get; set; }  public required string ImageUrl { get; set; } public required string Tag { get; set; } }
